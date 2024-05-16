b9bed6c2b1e73d53fe9c7450c0efec469f50bdf2
sahidDev09 <iamsahidofficial99@gmail.com>
2024-05-16 23:22:10 +0600

    final touch

--- src/Pages/BlogDetails.jsx
+++ src/Pages/BlogDetails.jsx
@@ -1,5 +1,4 @@
-/* eslint-disable no-unused-vars */
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link, useLoaderData } from "react-router-dom";
 import { AuthContext } from "../Provider/AuthProvider";
 import { Helmet } from "react-helmet";
@@ -10,6 +9,7 @@ import { toast } from "react-toastify";
 
 const BlogDetails = () => {
   const blogsDetails = useLoaderData();
+  const [comments, setComments] = useState([]);
 
   const { user } = useContext(AuthContext);
 
@@ -60,19 +60,34 @@ const BlogDetails = () => {
     }
   };
 
+  useEffect(() => {
+    const fetchComments = async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_API_URL}/comments/${blogsDetails._id}`
+        );
+        setComments(response.data);
+      } catch (error) {
+        console.error("Error fetching comments:", error);
+      }
+    };
+
+    fetchComments();
+  }, [blogsDetails._id]);
+
   return (
     <div className=" container mx-auto mt-14 p-2 md:p-0">
       <Helmet>
         <title>WriteWave | Blog_Details</title>
       </Helmet>
-      <h1 className=" md:text-4xl font-bold bg-blue-100 p-2 rounded-md">
+      <h1 className="md:text-4xl font-bold bg-blue-100 p-2 rounded-md">
         {title}
       </h1>
       <div className="md:flex gap-5 my-8 w-[100%]">
-        <div className=" md:w-[50%]">
-          <img className=" h-[65%] object-cover" src={imageurl} alt="" />
+        <div className="md:w-[50%]">
+          <img className="h-[65%] object-cover" src={imageurl} alt="" />
           <div className="">
-            <div className=" h-[25%]">
+            <div className="h-[25%]">
               <div className="flex gap-2 items-center mt-5">
                 <img
                   className="w-10 h-10 rounded-full p-1 border border-orange-500"
@@ -81,24 +96,24 @@ const BlogDetails = () => {
                 />
                 <div>
                   <h1>{user?.displayName}</h1>
-                  <h1 className=" text-gray-600">
+                  <h1 className="text-gray-600">
                     Comment will send from this user
                   </h1>
                 </div>
               </div>
-              <div className=" flex flex-col my-4">
-                <form onSubmit={handleComment} className=" flex flex-col mb-10">
+              <div className="flex flex-col my-4">
+                <form onSubmit={handleComment} className="flex flex-col mb-10">
                   <label htmlFor="">Message</label>
                   <textarea
                     name="comments"
                     required
-                    className=" border-2 h-full"
+                    className="border-2 h-full"
                     rows={4}
-                    placeholder=" Write your valueable comments..."></textarea>
+                    placeholder="Write your valuable comments..."></textarea>
                   <input
                     type="submit"
                     value="Send comment"
-                    className=" hover:bg-blue-700 cursor-pointer bg-blue-500 items-center flex p-3 w-fit my-2 text-white rounded-md"
+                    className="hover:bg-blue-700 cursor-pointer bg-blue-500 items-center flex p-3 w-fit my-2 text-white rounded-md"
                   />
                 </form>
               </div>
@@ -107,26 +122,26 @@ const BlogDetails = () => {
         </div>
         <div className="md:w-[50%]">
           <div>
-            <h1 className=" text-lg font-semibold text-blue-400">
+            <h1 className="text-lg font-semibold text-blue-400">
               Writer Information
             </h1>
-            <hr className=" my-2" />
-            <div className=" flex gap-4 items-center bg-slate-200 p-3 rounded-xl">
+            <hr className="my-2" />
+            <div className="flex gap-4 items-center bg-slate-200 p-3 rounded-xl">
               <img
-                className=" w-16 h-16 rounded-2xl"
+                className="w-16 h-16 rounded-2xl"
                 src={writerProfile}
                 alt=""
               />
               <div>
-                <h1 className=" font-semibold">
+                <h1 className="font-semibold">
                   Name :
-                  <span className=" text-blue-500 font-normal">
+                  <span className="text-blue-500 font-normal">
                     {writerName}
                   </span>
                 </h1>
-                <h1 className=" font-semibold">
+                <h1 className="font-semibold">
                   Email :
-                  <span className=" text-gray-500 font-normal">
+                  <span className="text-gray-500 font-normal">
                     {writerEmail}
                   </span>
                 </h1>
@@ -136,22 +151,22 @@ const BlogDetails = () => {
           <h1 className="text-lg font-semibold text-orange-400 my-2">
             Blog Details
           </h1>
-          <hr className=" my-2" />
-          <div className=" flex flex-col gap-2">
-            <h1 className=" font-semibold">
-              Category :<span className=" font-normal ml-1">{category}</span>
+          <hr className="my-2" />
+          <div className="flex flex-col gap-2">
+            <h1 className="font-semibold">
+              Category :<span className="font-normal ml-1">{category}</span>
             </h1>
             <div>
-              <h1 className=" font-semibold mb-2">
+              <h1 className="font-semibold mb-2">
                 Short Details:
-                <span className=" text-gray-600 font-normal">
+                <span className="text-gray-600 font-normal">
                   {short_description}
                 </span>
               </h1>
-              <div className=" bg-slate-200 p-3 rounded-md">
-                <h1 className=" font-semibold">
+              <div className="bg-slate-200 p-3 rounded-md">
+                <h1 className="font-semibold">
                   Full Details:
-                  <span className=" mr-1 font-normal text-gray-500 line-clamp-3 md:line-clamp-none">
+                  <span className="mr-1 font-normal text-gray-500 line-clamp-3 md:line-clamp-none">
                     {long_description}
                   </span>
                 </h1>
@@ -161,13 +176,36 @@ const BlogDetails = () => {
 
               {user?.email === writerEmail && (
                 <Link to={`/update/${_id}`}>
-                  <button className=" w-full bg-orange-500 hover:bg-orange-700 p-4 text-white rounded-md my-10">
+                  <button className="w-full bg-orange-500 hover:bg-orange-700 p-4 text-white rounded-md my-10">
                     Update Blog
                   </button>
                 </Link>
               )}
             </div>
           </div>
+        </div>
+      </div>
+
+      {/* Comment Section */}
+      <div className="comments w-full">
+        <h2 className=" font-semibold">Comments:</h2>
+        <div className=" flex flex-col gap-4 my-3">
+          {comments.map((comment) => (
+            <div className=" bg-slate-200 p-4 rounded-md" key={comment._id}>
+              <div className="flex gap-3 items-center">
+                <img
+                  className=" w-10 rounded-full"
+                  src={comment.owner_prfile}
+                  alt=""
+                />
+                <div>
+                  <h1>{comment.owner_name}</h1>
+                  <hr className=" w-full"/>
+                  <p className=" text-gray-600">{comment.comments}</p>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,10 +3,14 @@ import Marque from "../Components/Marque";
 import Newsletter from "../Components/Newsletter";
 import RecentsCards from "../Components/RecentPosts/RecentsCards";
 import OutTeam from "../Components/OutTeam";
+import { Helmet } from "react-helmet";
 
 const Home = () => {
   return (
     <div className=" m-2 md:m-0">
+       <Helmet>
+          <title>WriteWave | Home</title>
+        </Helmet>
       <Hero />
       <h1 className=" text-3xl text-center mt-10">Recent Blogs</h1>
       <RecentsCards />
